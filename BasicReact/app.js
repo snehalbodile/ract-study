799b5c339ce7f1f19f710d8d1d332a3eec6d1040
Snehal Bodile <contact.snehalbodile@gmail.com>
2025-03-26 15:59:05 +0530

    Episode-03-Laying the fiundation-Componant cerate

--- BasicReact/app.js
+++ BasicReact/app.js
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1",
-    {id:"heading"},
-    "Hello world from React js");
+// const heading = React.createElement("h1",
+//     {id:"heading"},
+//     "Hello world from React js");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+//     root.render(heading);
+
+//Class Element 
+const title = (
+       <h1 id="heading" className="heading">Namaste react By using JSX</h1>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+
+root.render(title);
+
+//React functional component
+
+const HeadingComponent1 = () => {
+    return  <h1 id="heading" className="heading">Namaste react By using JSX</h1>
+};
+
+const HeadingComponent2 = () => {
+  
+  return (<div id="container">
+        <HeadingComponent1/>
+        <h1 id="heading" className="heading">Namaste react By using component composition</h1>
+    </div>); 
+    
+};
+root.render(<HeadingComponent2/>);
