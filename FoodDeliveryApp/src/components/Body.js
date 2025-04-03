26a739ab25363c6f55493a687ddad39c93d0a182
Snehal Bodile <contact.snehalbodile@gmail.com>
2025-04-03 16:16:43 +0530

    Episode-6 Search functanality

--- FoodDeliveryApp/src/components/Body.js
+++ FoodDeliveryApp/src/components/Body.js
@@ -1,20 +1,59 @@
 import restrautList from "../utilis/utilis";
 import RestCard from "./RestCard";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 const Body = () => {
-    var [restaurants,setRestaurant] = useState(restrautList); 
+
+    var [restaurants,setRestaurant] = useState([]);
+    var [filterRestaurants,setFilterRestaurant] = useState([]);
+    var [searchText,setSearchText] = useState("");
+    useEffect(()=>{
+        fetchData();
+        },[]); 
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+        
+        const apiData = await data.json();
+        setRestaurant(apiData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setFilterRestaurant(apiData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+       // console.log(apiData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    }
     return (
         <div className="container">
-             <button type="button" onClick ={ ()=>{
-              const filterRest=  restaurants.filter((restaurant)=>{
-                    return restaurant.data.avgRating >3;
-                });
-                setRestaurant(filterRest);
-                console.log(filterRest);
-             }} className="btn btn-warning btn-sm float-left">sort</button>
+            <div className="row">
+                <div className="col-xs-6 col-md-4">
+                <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Search" id="txtSearch" value ={searchText} 
+                    onChange={(e)=>{
+                       setSearchText(e.target.value);
+                    }}
+                    />
+                    <div className="input-group-btn">
+                    <button className="btn btn-primary" type="submit"
+                        onClick={()=>{
+                           var filterData = restaurants.filter(
+                                (restaurant)=>{
+                                    return restaurant.info.name.toLowerCase().includes(searchText.toLowerCase());
+                                }
+                            );
+                            setFilterRestaurant(filterData);                          
+                        }}
+                    >
+                        <span className="glyphicon glyphicon-search"></span>
+                    </button>
+                    {/* <button type="button" onClick ={ ()=>{
+                        const filterRest=  restaurants.filter((restaurant)=>{
+                                return restaurant.info.avgRating >3;
+                            });
+                            setRestaurant(filterRest);
+                        }} className="btn btn-warning btn-sm float-left">sort</button> */}
+                    </div>
+                </div>
+                </div>
+            </div>
+           
             {  
-             restaurants.map((restaurant) => {
-                    return <RestCard key ={restaurant.data.uuid} restData = {restaurant}/>
+             filterRestaurants.map((restaurant) => {
+                    return <RestCard key ={restaurant.info.id} restData = {restaurant}/>
                 })
             }
            
--- FoodDeliveryApp/src/components/RestCard.js
+++ FoodDeliveryApp/src/components/RestCard.js
@@ -1,16 +1,14 @@
 import {REST_CDN} from "../utilis/constants";
 const RestCard = (prop) => {
-    console.log(prop);
     const {restData} = prop;
-    console.log(restData);
     return(
         <div className="card col-sm-4 rest-card">
             <img src={ REST_CDN +
-          restData.data.cloudinaryImageId} className="card-img-top rest-logo" alt="..."></img>
+          restData.info.cloudinaryImageId} className="card-img-top rest-logo" alt="..."></img>
         <div className="card-body">
-            <h4 className="card-title">{restData.data.name}</h4>
-            <p>{restData.data.cuisines.join(", ")}</p>
-            <button type="button" className="btn btn-success">{restData.data.avgRating} * </button>
+            <h4 className="card-title">{restData.info.name}</h4>
+            <p>{restData.info.cuisines.join(", ")}</p>
+            <button type="button" className="btn btn-success">{restData.info.avgRating} * </button>
         </div>
         </div>
     );
