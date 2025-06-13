2d5446460d5b3daddd7e7dca2eff96c60997a6a2
Snehal Bodile <contact.snehalbodile@gmail.com>
2025-06-13 17:56:34 +0530

    Create New app and add router

---
+++ netflix-app/src/components/Body.js
@@ -0,0 +1,22 @@
+
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import Browse from "./Browse";
+import Login from "./Login";
+
+const appRouter = createBrowserRouter([
+        {
+            path:"/",
+            element:<Login/>
+        },
+        {
+            path:"/browse",
+            element:<Browse/>
+        }
+    ]);
+
+const Body = ()=>{
+    return(
+       <RouterProvider router ={appRouter} />
+    );
+}
+  export default Body;
