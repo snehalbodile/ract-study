e525c370fc33cc7ccc7f0247fef89c1258cfb435
Snehal Bodile <contact.snehalbodile@gmail.com>
2025-06-15 20:50:55 +0530

    Configute firesbase and deploy proeject on firebase

---
+++ netflix-app/src/utilis/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-cKDzC3y8BCJrXboJp_kgfIGlVdcJj4M",
+  authDomain: "netflixgtp-56c6c.firebaseapp.com",
+  projectId: "netflixgtp-56c6c",
+  storageBucket: "netflixgtp-56c6c.firebasestorage.app",
+  messagingSenderId: "169454117092",
+  appId: "1:169454117092:web:50c75e00f1e347c6590cf1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
